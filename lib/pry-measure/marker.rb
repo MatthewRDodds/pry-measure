5a6c79afbbf0d44795290546c9bf91925ad4beec
Matthew Dodds <matthewrusselldodds@gmail.com>
2014-05-12 22:37:36 -0400

    Add showing of output and averaging of results

--- lib/pry-measure/marker.rb
+++ lib/pry-measure/marker.rb
@@ -1,10 +1,8 @@
-require "pry-measure/results"
-
 module PryMeasure
   class Marker
-    def self.measure(execution_times, arg_string, command)
-      tms = Benchmark.measure do
-        execution_times.times do
+    def self.measure(execution_times, arg_string, command, realtime=false, show_output=false, results=[])
+      execution_times.times do
+        results << Benchmark.measure do
           begin
             command.interpolate_string arg_string
           rescue Pry::RescuableException => ex
@@ -12,7 +10,23 @@ module PryMeasure
           end
         end
       end
-      Results.new tms
+
+      if show_output == true
+        unless realtime
+          puts "    user     system      total        real"
+        end
+        puts results
+      end
+
+      average = average_results(results, execution_times)
+      realtime ? average[:real] : average
+    end
+
+    def self.average_results(results, execution_times, response={})
+      [:real, :utime, :stime, :total].each do |key|
+        response.merge!(key =>(results.map(&key).inject(&:+) / execution_times.to_i).round(4)) rescue binding.pry
+      end
+      response
     end
   end
 end
--- lib/pry-measure/measure_command.rb
+++ lib/pry-measure/measure_command.rb
@@ -4,7 +4,7 @@ PryMeasure::Commands.create_command "pry-measure" do
   description "Measure a block of code using Ruby's benchmark module"
 
   banner <<-BANNER
-    Usage: measure [ -t <times> ] <block>
+    Usage: measure [ -t <times> -s ] <block>
 
     Calls the block for the given amount of times (default 500) and returns benchmark results.
   BANNER
@@ -20,6 +20,7 @@ PryMeasure::Commands.create_command "pry-measure" do
   end
 
   def options(opt)
+    opt.on :s, :show, "Shows default benchmark output"
     opt.on :t, :times, "Times to call block (default: 500)", optional: true, argument: true
   end
 
@@ -29,7 +30,9 @@ PryMeasure::Commands.create_command "pry-measure" do
       raise Pry::CommandError, "-t should be an integer" unless times_is_int?
     end
 
-    PryMeasure::Marker.measure @times.to_i, arg_string, self
+    output = !opts[:show].nil?
+
+    PryMeasure::Marker.measure @times.to_i, arg_string, self, false, output
   end
 
   def times_is_int?
